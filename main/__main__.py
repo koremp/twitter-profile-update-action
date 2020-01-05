9e283080399eea3f9422095f46d40b839effe1f0
Seonghyeon Kim <kim@seonghyeon.dev>
2020-01-05 17:47:36 +0900

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -2,4 +2,4 @@ from imperial_dateutil import ImperialDatingSystem
 
 if __name__ == '__main__':
     t = ImperialDatingSystem.now()
-    print(f"description=테라 기준 {str(t)}을 살아가고 있는 테크-프리스트.")
+    print(f'"description=테라 기준 {str(t)}을 살아가고 있는 테크-프리스트."')
