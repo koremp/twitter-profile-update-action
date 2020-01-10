f8500d0e9f4cd1bcbfe89dc64a07ffc9dc08ec62
Dokyun Lim <idk255@naver.com>
2020-01-10 15:51:25 +0900

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -2,4 +2,4 @@ from datetime import timedelta, datetime
 
 if __name__ == '__main__':
     days = datetime.now() - datetime(2019, 8, 25)
-    print(f'D+{(days.days)}/ 플밍 공부중/ FUB FREE')
+    print(f'description=D+{(days.days)}/ 플밍 공부중/ FUB FREE')
